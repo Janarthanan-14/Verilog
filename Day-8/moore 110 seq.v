fd25c6750c2c793f7e282e2dc1b87f2ee5ee5383
Janarthanan-14 <janarthanannraj@gmail.com>
2025-03-28 12:51:26 +0530

    Update moore 110 seq.v

--- Day-8/moore 110 seq.v
+++ Day-8/moore 110 seq.v
@@ -9,47 +9,47 @@ module moore_110_detector(input clk,rst,in,
   reg [1:0] state,next;
   always@(posedge clk or negedge rst)
     begin
-      if(!rst)
-        state <=s0;
-        else
-          state <= next;
-    end
+    if(!rst)
+   state <=s0;
+    else
+   state <= next;
+                  end
   always@(state or in)
     begin
-      case(state)
-        s0:
-          begin
-            out= 0;
-            if(in)
-              next=s1;
-            else
-              next=s0;
-          end
-        s1:
-          begin
-          out=1;
-          if(in)
-            next=s2;
-          else
-            next=s0;
-        end
-        s2:
-          begin
-            out=2;
-            if(in)
-              next=s3;
-            else
-              next =s0;
-          end
-        s3:
-          begin
-            out=3;
-            if(in)
-              next=s0;
-            else
-              next=s1;
-          end
-        endcase
-    end
-        endmodule
+    case(state)
+    s0:
+    begin
+    out= 0;
+    if(in)
+    next=s1;
+    else
+    next=s0;
+                end
+    s1:
+    begin
+    out=1;
+    if(in)
+    next=s2;
+    else
+    next=s0;
+                 end
+     s2:
+     begin
+      out=2;
+      if(in)
+      next=s3;
+       else
+       next =s0;
+                 end
+      s3:
+      begin
+      out=3;
+      if(in)
+      next=s0;
+       else
+      next=s1;
+                  end
+                  endcase
+                  end
+                  endmodule
         
